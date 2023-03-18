7dc20484cd4a0f3c2d34311576409d2362790d44
hknhtpgl48 <hkn.hatipolu48@gmail.com>
2023-03-18 23:01:16 +0300

    segue added

--- Navigation Controller Example/Screens/Main/Controller/ViewController.swift
+++ Navigation Controller Example/Screens/Main/Controller/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func goToProfileButtonPressed(_ sender: UIButton){
+        performSegue(withIdentifier: "Profile", sender: self)
+    }
 
 
 }
